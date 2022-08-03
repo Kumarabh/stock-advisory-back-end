4efbb58faf64d52fc0a61e21b9cd005148a9e175
Abhishek Kumar <Abhishek.km2050@gmail.com>
2022-08-03 18:39:30 +0530

    commit-added dropdown data

--- config/db.js
+++ config/db.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
+const advisoryDropdownModel = require('../models/advisory-dropdown.model');
 
 const connectDB = () => {
-    mongoose.connect(process.env.DATABASE, 
-        {useNewUrlParser: true, useUnifiedTopology: true},
-        () => console.log('DB Connected.'))
+    mongoose.connect(process.env.DATABASE,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => console.log('DB Connected.')).then(data => {
+            const data =
+                { "callTypes": ["INTRADAY", "DELIVERY"], "action": ["BUY", "SELL"], "category": ["EQUITY", "FUTURES", "OPTIONS"], "nameOfShare": ["Stock A", "Stock B", "Stock C"] }
+            await advisoryDropdownModel.create(data);
+
+        })
 }
 
 module.exports = connectDB;
