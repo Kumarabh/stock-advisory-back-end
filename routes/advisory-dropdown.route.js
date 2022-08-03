4627da983bceddd27a6a468a438b24fa4026414b
Abhishek Kumar <Abhishek.km2050@gmail.com>
2022-08-03 18:47:08 +0530

    commit: dropdown-fix - Abhishek

--- routes/advisory-dropdown.route.js
+++ routes/advisory-dropdown.route.js
@@ -3,6 +3,10 @@ const advisoryDropdownModel = require('../models/advisory-dropdown.model');
 const router = express.Router();
 router.get('', async (req, res) => {
 
+    const data =
+                { "callTypes": ["INTRADAY", "DELIVERY"], "action": ["BUY", "SELL"], "category": ["EQUITY", "FUTURES", "OPTIONS"], "nameOfShare": ["Stock A", "Stock B", "Stock C"] }
+            await advisoryDropdownModel.create(data);
+            
     const dbresult = await advisoryDropdownModel.find();
     !dbresult && res.status(404).json('Data is not available');
 
