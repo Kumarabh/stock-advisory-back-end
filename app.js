2406bdb234cdb260dd23b80d904e33be4148288c
Abhishek Kumar <Abhishek.km2050@gmail.com>
2022-08-05 16:26:43 +0530

    commit - cors error fix - Abhishek

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 require('dotenv').config({path: './config/config.env'})
+const cors = require('cors');
 const connectDB = require('./config/db');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const morgan = require('morgan');
 const { default: helmet } = require('helmet');
 connectDB();
 
-app.use(cors({origin:'http://143.110.240.38'}))
+app.use(cors())
 app.use(morgan('common'))
 app.use(helmet())
 app.use(bodyParser.json());
