24b47b61fa669c466198bca9e157385f40f2fb36
Abhishek Kumar <Abhishek.km2050@gmail.com>
2022-08-08 13:56:38 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = () => {
     mongoose.connect(process.env.DATABASE, 
-        {useNewUrlParser: true, useUnifiedTopology: true},
+        {},
         () => console.log('DB Connected.'))
 }
 
