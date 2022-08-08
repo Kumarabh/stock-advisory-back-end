41eee13f4e3a39dfe515629eb09b167af39bc09f
Abhishek Kumar <Abhishek.km2050@gmail.com>
2022-08-08 13:23:12 +0530

    commit - timeout error fix - Abhishek

--- config/db.js
+++ config/db.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = () => {
     mongoose.connect(process.env.DATABASE, 
-        {useNewUrlParser: true, useUnifiedTopology: true},
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,   },6000000
+            ,
         () => console.log('DB Connected.'))
 }
 
